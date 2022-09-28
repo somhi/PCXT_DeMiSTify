f48fb62426c94760c0501cb183794afc0bd56ca8
somhi <somhix@gmail.com>
2022-09-28 23:44:19 +0200

    added cga 64kb mode. Improves some games like arkanoid and gods, but Prince looks worst

--- neptuno/defs.v
+++ neptuno/defs.v
@@ -1,4 +1,5 @@
 //`define NO_CGA
 //`define CGA_16
-`define CGA_32			// 119 / 260 ( 65 % )
-//`define CGA_128		// 399 / 260
+`define CGA_32		    // 119 / 260 ( 65 % )
+//`define CGA_64		// 182 / 260
+//`define CGA_128		// 299 / 260
