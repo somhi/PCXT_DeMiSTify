fbfc54e0491f5c50fe9593835a0cb58e5555c354
somhi <somhix@gmail.com>
2022-10-01 22:01:45 +0200

    added defines EMBED_8088_ROM, NO_ADLIB, so now is possible CGA_32 in atlas cyc1000, and maybe Sidi and MiST

--- atlas_cyc/defs.v
+++ atlas_cyc/defs.v
@@ -1,6 +1,8 @@
+`define EMBED_8088_ROM
+`define NO_ADLIB
 `define NO_MDA
 //`define NO_CGA
-`define CGA_16      64 / 66 ( 97 % )
-//`define CGA_32
+//`define CGA_16      64 / 66 ( 97 % )
+`define CGA_32		//M9Ks 55 / 66 ( 83 % ) with EMBED_8088_ROM, NO_ADLIB, NO_MDA
 //`define CGA_64
 //`define CGA_128
