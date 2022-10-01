2e8a168486b1f144fbe46a4d956f15933a5b7b38
somhi <somhix@gmail.com>
2022-10-01 18:38:33 +0200

    ascii-hex script and other minor changes

---
+++ SW/make_splash_ascii-hex.py
@@ -0,0 +1,40 @@
+fp = open('splash.txt')
+lines = fp.readlines()
+fp.close()
+
+attr = ' 07 ' # char attribute
+cols = 80 # columns
+rows = 25 # rows
+out = '\x02 $A0000,\n'
+
+rows_count = rows
+for line in lines:
+    rows_count = rows_count -1
+    line = line.replace('\n', '')
+    col_count = cols
+    for c in line:
+        col_count = col_count - 1
+        hex_str = hex(ord(c))		
+        out = out + hex_str.replace('0x', '').upper() + attr
+        if col_count == 0:
+            break
+    while (col_count > 0):
+        col_count = col_count - 1
+        out = out + '20' + attr
+    
+    if rows_count == 0:
+        break
+        
+while (rows_count > 0):    
+    rows_count = rows_count -1
+    col_count = cols
+    while (col_count > 0):
+        col_count = col_count - 1
+        out = out + '20' + attr
+
+out = out.strip()
+out = out + '\n\x03\n$SDB8B,\n'
+
+fp = open('splash.hex', 'w+')
+fp.write(out)
+fp.close()
