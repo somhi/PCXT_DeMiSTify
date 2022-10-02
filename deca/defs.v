9053a89d57d3a78357e47404b75714f3107d9cc3
somhi <somhix@gmail.com>
2022-10-02 11:58:22 +0200

    Readmes; DECA working; OSD changes

--- deca/defs.v
+++ deca/defs.v
@@ -3,4 +3,6 @@
 //`define CGA_16
 //`define CGA_32			
 //`define CGA_64
-`define CGA_128			//179 / 182
+`define CGA_128			//M9Ks  179 / 182  ( 98 % )
+// Total logic elements	16,244 / 49,760 ( 33 % )
+
