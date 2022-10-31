38024ff294eb7d8df2da88fdffc1689e0cdde6e6
somhi <somhix@gmail.com>
2022-10-31 18:43:43 +0100

    de10lite port

---
+++ de10lite/defs.v
@@ -0,0 +1,11 @@
+//`define EMBED_8088_ROM
+//`define NO_COM2
+//`define NO_ADLIB
+//`define NO_MDA
+//`define NO_CGA
+//`define CGA_16
+//`define CGA_32			
+//`define CGA_64
+`define CGA_128			//
+						//
+
