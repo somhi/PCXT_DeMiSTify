da2c6f24a47ee2a403b04955528e678ee18f1313
somhi <somhix@gmail.com>
2023-02-03 23:52:52 +0100

    deca_mist target testing

---
+++ deca_mist/README.md
@@ -0,0 +1,75 @@
+# [IBM PC/XT](https://en.wikipedia.org/wiki/IBM_Personal_Computer_XT)  [DeMiSTified](https://github.com/robinsonb5/DeMiSTify) - Deca MiST port
+
+02/02/23 Deca MiST port DeMiSTified by @somhi from original MiSTer PCXT core  https://github.com/spark2k06/PCXT_MiSTer
+
+Read the main [Readme](https://github.com/somhi/PCXT_DeMiSTify) also.
+
+### STATUS
+
+* CGA VRAM with 128 kB for all Tandy games now available.
+
+* **UART port needs to be used to load the OS through the serdrive app.** Load OS using Serial Rx/Tx cable.
+
+**Now compatible with [Deca Retro Cape 2](https://github.com/somhi/DECA_retro_cape_2)** (new location for 3 pins of old SDRAM modules). Otherwise see pinout below to connect everything through GPIOs.
+
+## **Features:**
+
+* VGA 444 video output is available through GPIO. 
+* Audio I2S Line out (3.5 jack green connector) 
+
+**Additional hardware required**:
+
+- SDRAM module
+  - Tested with 32 MB SDRAM board for MiSTer (extra slim) XS_2.2 ([see connections](https://github.com/SoCFPGA-learning/DECA/tree/main/Projects/sdram_mister_deca))
+  - Tested with a dual memory module v1.3 with 3 pins ([see connections](https://github.com/SoCFPGA-learning/DECA/tree/main/Projects/sdram_mister_deca) + [3pins](https://github.com/DECAfpga/DECA_board/blob/main/Sdram_mister_deca/README_3pins.md))
+- PS/2 Keyboard connected to GPIO
+
+
+
+### Compile the project in Quartus:
+
+Project already has the DemiSTify firmware already generated so if you have cloned recursively then you can open the project with Quartus:
+
+```sh
+git clone  --recursive https://github.com/somhi/PCXT_DeMiSTify
+
+#check comments on top of /deca_mist/deca_mist_top.vhd in case additional actions are needed
+
+#Load project in Quartus from /deca_mist/PCXT_deca_mist.qpf
+```
+
+
+
+### Instructions to compile the project for a specific board:
+
+```sh
+git clone https://github.com/somhi/PCXT_DeMiSTify
+cd PCXT_DeMiSTify
+#Do a first make (will finish in error) but it will download missing submodules 
+make
+cd DeMiSTify
+#Create file site.mk in DeMiSTify folder 
+cp site.template site.mk
+#Edit site.mk and add your own PATHs to Quartus (Q18)
+gedit site.mk
+#Go back to root folder and do a make with board target (deca, deca_mist, neptuno, uareloaded, atlas_cyc). If not specified it will compile for all targets.
+cd ..
+make BOARD=deca_mist
+#when asked just accept default settings with Enter key
+```
+
+After that you can:
+
+* Flash bitstream directly from [command line](https://github.com/DECAfpga/DECA_binaries#flash-bitstream-to-fgpa-with-quartus)
+* Load project in Quartus from /deca_mist/PCXT_deca_mist.qpf
+
+### Pinout connections:
+
+![pinout_deca](pinout_deca.png)
+
+Mouse, joystick, MIDI are not used in this core
+
+UART port is required to be used to load the OS through the serdrive app.
+
+For 444 video DAC use all VGA pins. For 333 video DAC connect MSB from addon to MSB of location assignment (e.g. connect pin VGAR2 from Waveshare addon to VGA_R[3] Deca pin).
+
---
+++ deca_mist/deca_mist_top.sv
@@ -0,0 +1,164 @@
+//
+//
+// https://github.com/mist-devel
+// 
+// Copyright (c) 2015 Till Harbaum <till@harbaum.org> 
+// 
+// This source file is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version. 
+// 
+// This source file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License 
+// along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+//
+
+module deca_mist_top (
+   input  	 MAX10_CLK1_50,
+
+	// LED outputs
+   output [7:0]   LED, // LED Yellow
+	
+   // SDRAM interface
+   inout  [15:0]  DRAM_DQ, // SDRAM Data bus 16 Bits
+   output [12:0]  DRAM_ADDR, // SDRAM Address bus 13 Bits
+   output 	      DRAM_LDQM, // SDRAM Low-byte Data Mask
+   output 	      DRAM_UDQM, // SDRAM High-byte Data Mask
+   output 	      DRAM_WE_N, // SDRAM Write Enable
+   output 	      DRAM_CAS_N, // SDRAM Column Address Strobe
+   output         DRAM_RAS_N, // SDRAM Row Address Strobe
+   output 	      DRAM_CS_N, // SDRAM Chip Select
+   output [1:0]   DRAM_BA, // SDRAM Bank Address
+   output 	      DRAM_CLK, // SDRAM Clock
+   output 	      DRAM_CKE, // SDRAM Clock Enable
+
+   // SPI interface to arm io controller
+   output 	 SPI_MISO_WSBD, //SPI_DO,
+   input 	 SPI_MOSI,      //SPI_DI,
+   input 	 SPI_SCLK_DABD, //SPI_SCK,
+   input 	 SPI_CS2,       //SPI_SS2,
+   input 	 SPI_CS0_CLKBD, //SPI_SS3,
+   input 	 SPI_CS1,       //CONF_DATA0, 
+
+   //output 	 AUDIO_L, // sigma-delta DAC output left
+   //output 	 AUDIO_R, // sigma-delta DAC output right
+
+   // Audio DAC DECA
+	output wire i2sMck,			//AUDIO_MCLK
+	output wire i2sSck,			//AUDIO_BCLK
+	output wire i2sLr,			//AUDIO_WCLK
+	output wire i2sD,			//AUDIO_DIN_MFP1
+	inout  wire	AUDIO_GPIO_MFP5,
+	input  wire	AUDIO_MISO_MFP4,
+	inout  wire	AUDIO_RESET_n,
+	output wire AUDIO_SCLK_MFP3,
+	output wire AUDIO_SCL_SS_n,
+	inout  wire	AUDIO_SDA_MOSI,
+	output wire AUDIO_SPI_SELECT,
+
+   //VIDEO
+   output 	 VGA_HS,
+   output 	 VGA_VS,
+   output [3:0]  VGA_R,
+   output [3:0]  VGA_G,
+   output [3:0]  VGA_B,
+
+   //UART
+   input     UART_RXD,
+   output    UART_TXD
+);
+
+wire [7:0]  r_aux, g_aux, b_aux;	
+wire [15:0] dac_l, dac_r;
+
+assign LED[7:1]='1;
+
+PCXT guest2       //module name has been deliveratelly changed from guest to guest2 so in constraints.sdc file
+                  //set topmodule "guest|"    is set incorrectly but is has been proven that it works better in this way
+                  //TODO revise constraints for SiDi and MiST boards
+(
+   .CLOCK_27 	(MAX10_CLK1_50),
+   .RESET_N    (1'b1),        //very important to pass this reset signal
+   .LED      	(~LED[0]),
+
+   .SDRAM_DQ	(DRAM_DQ),	
+   .SDRAM_A		(DRAM_ADDR),
+   .SDRAM_DQML	(DRAM_LDQM),
+   .SDRAM_DQMH	(DRAM_UDQM),
+   .SDRAM_nWE	(DRAM_WE_N),
+   .SDRAM_nCAS	(DRAM_CAS_N),
+   .SDRAM_nRAS	(DRAM_RAS_N),
+   .SDRAM_nCS	(DRAM_CS_N),
+   .SDRAM_BA	(DRAM_BA),
+   .SDRAM_CLK	(DRAM_CLK),
+   .SDRAM_CKE	(DRAM_CKE),
+                      
+   .SPI_DO		(SPI_MISO_WSBD),
+   .SPI_DI		(SPI_MOSI),
+   .SPI_SCK		(SPI_SCLK_DABD),
+   .SPI_SS2		(SPI_CS2),
+   .SPI_SS3		(SPI_CS0_CLKBD),
+// .SPI_SS4		(SPI_SS4),
+   .CONF_DATA0	(SPI_CS1),
+
+// .AUDIO_L  	(AUDIO_L),
+// .AUDIO_R  	(AUDIO_R),
+
+   .DAC_L      (dac_l),
+   .DAC_R      (dac_r),
+
+   .VGA_HS		(VGA_HS),
+   .VGA_VS		(VGA_VS),
+   .VGA_R		(r_aux),
+   .VGA_G		(g_aux),
+   .VGA_B		(b_aux),
+
+   .UART_RX		(UART_RXD),	
+   .UART_TX		(UART_TXD)	
+
+);
+
+// VIDEO bits assignation
+assign VGA_R = r_aux[7:4];
+assign VGA_G = g_aux[7:4];
+assign VGA_B = b_aux[7:4];
+
+
+// AUDIO CODEC
+wire   RESET_DELAY_n;
+assign RESET_DELAY_n = 1'b1;     // BUTTON;
+
+// Audio DAC DECA Output assignments
+assign AUDIO_GPIO_MFP5  = 1'b1;  // GPIO
+assign AUDIO_SPI_SELECT = 1'b1;  // SPI mode
+assign AUDIO_RESET_n    = RESET_DELAY_n;    
+
+// AUDIO CODEC SPI CONFIG
+// I2S mode; fs = 48khz; MCLK = 24.567Mhz x 2
+AUDIO_SPI_CTL_RD u1 (
+	.iRESET_n	(RESET_DELAY_n  ), 	
+	.iCLK_50	   (MAX10_CLK1_50  ),  //50Mhz clock
+	.oCS_n		(AUDIO_SCL_SS_n ),  //SPI interface mode chip-select signal
+	.oSCLK		(AUDIO_SCLK_MFP3),  //SPI serial clock
+	.oDIN		   (AUDIO_SDA_MOSI ),  //SPI Serial data output
+	.iDOUT		(AUDIO_MISO_MFP4)   //SPI serial data input
+);
+
+audio_top audio_top  
+(
+	.clk_50MHz (MAX10_CLK1_50),
+	.dac_MCLK  (i2sMck),
+	.dac_LRCK  (i2sLr),
+	.dac_SCLK  (i2sSck),
+	.dac_SDIN  (i2sD),
+	.L_data    (dac_l),
+	.R_data    (dac_r)
+);
+
+
+endmodule
