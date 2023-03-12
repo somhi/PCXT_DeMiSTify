f488535ab4cda32f125cd07d92693333eef2ba4c
somhi <somhix@gmail.com>
2023-03-12 22:40:07 +0100

    KFPS2KB_direct used now for all ports

--- rtl/KFPC-XT/HDL/Peripherals.sv
+++ rtl/KFPC-XT/HDL/Peripherals.sv
@@ -425,32 +425,6 @@ module PERIPHERALS #(
     end
 
 
-    `ifdef MIST_SIDI
-    KFPS2KB u_KFPS2KB 
-    (
-        // Bus
-        .clock                      (clock),
-        .peripheral_clock           (peripheral_clock),
-        .reset                      (reset),
-
-        // PS/2 I/O
-        .device_clock               (ps2_clock | lock_recv_clock),
-        .device_data                (ps2_data),
-
-        // I/O
-        .irq                        (keybord_irq),
-        .keycode                    (keycode),
-        .reset_keyboard             (~prev_ps2_reset_n & ps2_reset_n),
-        .clear_keycode              (clear_keycode),
-        .pause_core                 (pause_core)
-    );
-
-    // No need to send any command to the PS2 keyboard, answer to reset is created locally
-    assign ps2_clock_out = 1'b1;
-    assign ps2_data_out = 1'b1;
-
-    `else 
-
     KFPS2KB_direct u_KFPS2KB 
     (
         // Bus
@@ -497,7 +471,6 @@ module PERIPHERALS #(
             ps2_clock_out = ps2_send_clock;		// kitune-san change for direct keyboard interface
     end
 
-    `endif
 
     // Convert Tandy scancode
     Tandy_Scancode_Converter u_Tandy_Scancode_Converter 
