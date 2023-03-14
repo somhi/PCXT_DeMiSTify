437c7afca5b83b51dc9893fa897c2e52ae2fecd9
somhi <somhix@gmail.com>
2023-03-14 20:57:37 +0100

    unified KFPS2KB module for all ports

--- rtl/KFPC-XT/HDL/Peripherals.sv
+++ rtl/KFPC-XT/HDL/Peripherals.sv
@@ -434,12 +434,6 @@ module PERIPHERALS #(
             prev_ps2_reset_n <= ps2_reset_n;
     end
 
-
-    //Local AA response is generated with signal reset_keyboard in KFPS2KB modules
-    //Without local AA response MiST is getting message errors in YUKO ST and deMiSTified ports also with speeds > 4.77 MHz.
-
-    `ifdef MIST_SIDI
-    
     KFPS2KB u_KFPS2KB 
     (
         // Bus
@@ -459,30 +453,9 @@ module PERIPHERALS #(
         .pause_core                 (pause_core)
     );
 
-    `else 
-
-    KFPS2KB_direct u_KFPS2KB 
-    (
-        // Bus
-        .clock                      (clock),
-        .peripheral_clock           (peripheral_clock),
-        .reset                      (reset),
-
-        // PS/2 I/O
-        .device_clock               (ps2_clock | lock_recv_clock),
-        .device_data                (ps2_data),
-
-        // I/O
-        .irq                        (keybord_irq),
-        .keycode                    (keycode),
-        .reset_keyboard             (~prev_ps2_reset_n & ps2_reset_n),    
-        .clear_keycode              (clear_keycode),
-        .pause_core                 (pause_core)
-    );
-
-    `endif
-
     // No need to send any command to the PS2 keyboard, answer to reset is created locally
+    // Local AA response is generated with signal reset_keyboard in KFPS2KB module
+    // Without local AA response there is keyboard controller error in diagnostic ROMs and YUKO ST BIOS
     assign ps2_clock_out = 1'b1;
     assign ps2_data_out = 1'b1;
 
