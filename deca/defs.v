30804ef94dc4acf03b4f7c4ac935da7eab690f3e
somhi <somhix@gmail.com>
2023-03-20 00:03:41 +0100

    updated defs.v for Deca board

--- deca/defs.v
+++ deca/defs.v
@@ -8,7 +8,7 @@
 `define NO_CREDITS
 //`define NO_CGA		//if not CGA, only MDA is available
 //`define CGA_16
-//`define CGA_32			
+`define CGA_32			
 //`define CGA_64
-`define CGA_128			//M9Ks  174 / 182  ( 100 % )  with NO_CREDITS
+//`define CGA_128			//M9Ks  174 / 182  ( 100 % )  with NO_CREDITS
 
