35b5e307d1039f255ca12a05ac0cb0ff238c557b
somhi <somhix@gmail.com>
2024-03-23 00:40:51 +0100

    added poseidon-10CL120 target

---
+++ demistify/poseidon-10CL120/README.md
@@ -0,0 +1,26 @@
+# [IBM PC/XT](https://en.wikipedia.org/wiki/IBM_Personal_Computer_XT) - Poseidon GX150 MiST port
+
+15/12/23 Poseidon GX150 MiST port by @somhi from original MiSTer PCXT core  https://github.com/MiSTer-devel/PCXT_MiSTer by @spark2k06
+
+Read the main [Readme](https://github.com/somhi/PCXT_DeMiSTify) also.
+
+### MiST firmware
+
+Make sure you have a MiST [firmware](https://github.com/mist-devel/mist-firmware) version later than 24/03/23.
+
+### STATUS
+
+* Check file defs.v to verify and check disabled options.
+
+  
+
+### Compile the project in Quartus:
+
+Clone the repository recursively and open the project with Quartus:
+
+```sh
+git clone  --recursive https://github.com/somhi/PCXT_DeMiSTify
+
+#Load PCXT_*.qpf project file in Quartus 
+```
+
---
+++ demistify/poseidon-10CL120/poseidon_top.sv
@@ -0,0 +1,114 @@
+
+module poseidon_top (
+	input         CLOCK_50,
+	input         KEY0,
+	output        LED,
+
+	output  [5:0] VGA_R,
+	output  [5:0] VGA_G,
+	output  [5:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+
+`ifdef USE_HDMI
+	output        HDMI_RST,
+	output  [7:0] HDMI_R,
+	output  [7:0] HDMI_G,
+	output  [7:0] HDMI_B,
+	output        HDMI_HS,
+	output        HDMI_VS,
+	output        HDMI_PCLK,
+	output        HDMI_DE,
+	output        HDMI_SDA,
+	output        HDMI_SCL,
+`endif
+
+	input         SPI_SCK,
+	inout         SPI_DO,
+	input         SPI_DI,
+	input         SPI_SS2,    // data_io
+	input         SPI_SS3,    // OSD
+	input         CONF_DATA0, // SPI_SS for user_io
+`ifndef NO_DIRECT_UPLOAD
+	input         SPI_SS4,
+`endif
+
+	output [12:0] SDRAM_A,
+	inout  [15:0] SDRAM_DQ,
+	output        SDRAM_DQML,
+	output        SDRAM_DQMH,
+	output        SDRAM_nWE,
+	output        SDRAM_nCAS,
+	output        SDRAM_nRAS,
+	output        SDRAM_nCS,
+	output  [1:0] SDRAM_BA,
+	output        SDRAM_CLK,
+	output        SDRAM_CKE,
+
+	output        AUDIO_L,
+	output        AUDIO_R,
+`ifdef I2S_AUDIO
+	output        I2S_BCK,
+	output        I2S_LRCK,
+	output        I2S_DATA,
+`endif
+`ifdef USE_AUDIO_IN
+	input         AUDIO_IN,
+`endif
+	input         UART_RX,
+	output        UART_TX
+);
+
+
+wire [7:0]  r_aux, g_aux, b_aux;	
+// wire  [1:0] COMPOSITE_OUT;
+// wire UART_CTS, UART_RTS;
+
+PCXT guest
+(
+ .CLOCK_27   (CLOCK_50),
+ .RESET_N    (1'b1),        //1'b1 very important to pass this reset signal
+ .LED      	 (~LED),
+
+ .SDRAM_DQ	(SDRAM_DQ),	
+ .SDRAM_A	(SDRAM_A),
+ .SDRAM_DQML(SDRAM_DQML),
+ .SDRAM_DQMH(SDRAM_DQMH),
+ .SDRAM_nWE	(SDRAM_nWE),
+ .SDRAM_nCAS(SDRAM_nCAS),
+ .SDRAM_nRAS(SDRAM_nRAS),
+ .SDRAM_nCS	(SDRAM_nCS),
+ .SDRAM_BA	(SDRAM_BA),
+ .SDRAM_CLK	(SDRAM_CLK),
+ .SDRAM_CKE	(SDRAM_CKE),
+		 			
+ .SPI_DO	 (SPI_DO),
+ .SPI_DI	 (SPI_DI),
+ .SPI_SCK	 (SPI_SCK),
+ .SPI_SS2	 (SPI_SS2),
+ .SPI_SS3	 (SPI_SS3),
+ .SPI_SS4	 (SPI_SS4),
+ .CONF_DATA0 (CONF_DATA0),
+
+ .I2S_BCK	 (I2S_BCK),
+ .I2S_LRCK	 (I2S_LRCK),
+ .I2S_DATA	 (I2S_DATA),
+
+ .VGA_HS	 (VGA_HS),
+ .VGA_VS	 (VGA_VS),
+ .VGA_R	 	 (r_aux),
+ .VGA_G		 (g_aux),
+ .VGA_B		 (b_aux),
+
+ .UART_RX	 (UART_RX),	
+ .UART_TX	 (UART_TX)
+//  .UART_CTS   (UART_CTS),
+//  .UART_RTS   (UART_RTS)
+);
+
+// VIDEO bits assignation
+assign VGA_R = r_aux[7:2];
+assign VGA_G = g_aux[7:2];
+assign VGA_B = b_aux[7:2];
+
+endmodule
